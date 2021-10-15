b2d28ac9ac0fe3664131bf020e5ac4bf8abd9521
Juan Sebastian Victorino Rojas <jvictorino@unal.edu.co>
2021-10-15 09:00:36 -0500

    Hola mundo y Variables 15 Octubre

---
+++ 2021-10-15-15Int/holamundo.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+  std::cout<<"Hola Mundo Crachz";
+  int a;
+  float x;
+  double y=0.0;
+  std::cout << "a= " << a << "\n";
+  std::cout << "x= " << x << "\n";
+  std::cout << "y= " << y << "\n";
+  
+  return 0;
+}
